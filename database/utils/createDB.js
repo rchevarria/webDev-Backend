9830505dc7c75ff0c3e4a774c3ed277444c331d7
Ryan Chevarria <Ryan.Chevarria64@myhunter.cuny.edu>
2022-12-17 12:56:13 -0500

    database creation

--- database/utils/createDB.js
+++ database/utils/createDB.js
@@ -1,1 +1,27 @@
+const pgtools = require('pgtools');
+const {dbName, dbUser, dbPwd} = require('./configDB');
 
+const config = {
+    user: dbUser,
+    host: 'localhost',
+    port: 5433,
+    password: dbPwd
+};
+
+//Database creation
+const createDB = async () => {
+    try{
+        await pgtools.createdb(config, dbName);
+        console.log('Sucessfully created the database: ${dbName}!');
+    } catch (err){
+        if(err.name === 'duplicate_database'){
+            console.log('Database ${dbName} already exists');
+            return;
+        }else{
+            console.error('createDB error:', err);
+            process.exit(1);
+        }
+    }
+}
+
+module.exports = createDB;
